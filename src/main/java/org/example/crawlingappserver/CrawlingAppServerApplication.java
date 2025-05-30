3e4c216ea1e274611fe08ab4f16164c3496fdc0e
clcok <ekfrha24@gmail.com>
2025-05-30 23:49:39 +0900

    init: start project

---
+++ src/main/java/org/example/crawlingappserver/CrawlingAppServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.crawlingappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlingAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrawlingAppServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/crawlingappserver/CrawlingAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.crawlingappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrawlingAppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
